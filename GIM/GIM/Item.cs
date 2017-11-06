b7b9611f40652661bf28a637246602d628e2bddf
RootFive <cadillacshine@hotmail.com>
2017-11-06 09:22:55 +0000

    Class definitions

---
+++ GIM/GIM/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIM {
+    class Item {
+        public int itemID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public double quantity { get; set; }
+        public string unitOfMeasure { get; set; }
+        public double price { get; set; }
+        public double extension { get; set; }
+        public string status { get; set; }
+        //some date comes here
+        public bool active { get; set; }
+
+    }
+}
---
+++ GIM/GIM/UnitOfMeasure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIM {
+    class UnitOfMeasure {
+        public int unitOfMeasureID { get; set; }
+        public string uom { get; set; }
+        public string shortName { get; set; }
+        public bool active { get; set; }
+    }
+}
